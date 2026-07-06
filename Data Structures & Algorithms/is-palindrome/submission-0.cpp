f8a9810e258424eedfce2d45b25eab525cefd74d
Rakshit Goyal <rakshitgoyal770@gmail.com>
2026-07-06 14:33:34 +0530

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        // 2 possibilites - odd/even
+        int ed = s.size() - 1;
+        int st = 0;
+
+        while(ed >  st){
+            if( !(s[st] >= 'a' && s[st] <= 'z') && !(s[st] >= 'A' && s[st] <= 'Z') && !(s[st] >= '0' && s[st] <= '9')){
+                st++;
+                continue;
+            }
+            if( !(s[ed] >= 'a' && s[ed] <= 'z') && !(s[ed] >= 'A' && s[ed] <= 'Z') && !(s[ed] >= '0' && s[ed] <= '9')){
+                ed --;
+                continue;
+            }
+            if(tolower(s[ed]) == tolower(s[st])){
+                ed --;
+                st ++;
+                cout << s[ed];
+                cout << s[st];
+            }
+            else{
+                cout << s[ed];
+                cout << s[st];
+                return false;
+            }
+
+        }
+        return true;
+        
+    }
+};
