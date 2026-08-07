e5aeaa5e91838c812cd6948a1271ad400674ba89
Rakshit Goyal <rakshitgoyal770@gmail.com>
2026-08-07 15:21:50 +0530

    Add: count-connected-components - submission-0

---
+++ Data Structures & Algorithms/count-connected-components/submission-0.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    unordered_map<int, vector<int>> mp;
+    unordered_set<int> visited;
+    int count = 0;
+
+    void dfs(int i, int prev){
+        if(visited.count(i)){
+            return;
+        }
+
+        visited.insert(i);
+
+        if(mp[i].empty()){
+            return;
+        }
+
+        for(auto adj : mp[i]){
+            if(i == prev){
+                continue;
+            }
+            dfs(adj, i);
+        }
+
+        return;
+    }
+
+    int countComponents(int n, vector<vector<int>>& edges) {
+        for(auto ed : edges){
+            mp[ed[0]].push_back(ed[1]);
+            mp[ed[1]].push_back(ed[0]);
+        }
+
+        for(int i = 0; i < n; i++){
+            if(visited.count(i)){
+                continue;
+            }
+            else{
+                count++;
+                dfs(i, -1);
+            }
+        }
+
+        return count;
+
+    }
+};
